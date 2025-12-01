de994f18ed406c322f25834ca37b8a6e25700d13
DennisStaack <dennis@familie-staack.de>
2025-12-01 17:34:39 +0100

    i dont know what im doing

--- days/day1/src/main.rs
+++ days/day1/src/main.rs
@@ -2,11 +2,10 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 fn main() {
-    task1();
     task2();
 }
 
-fn task1() {
+fn task1() -> i32 {
     //let path = "D:/code/repos/aoc/aocrust25/inputFiles/day1input.txt";
     let path = "/home/djs/docs/projects/adventOfCode/25/aocrust25/inputFiles/day1input.txt";
     let input = File::open(path).expect("File not found");
@@ -40,6 +39,7 @@ fn task1() {
     }
 
     println!("Task1 Pw: {}", count);
+    return count;
 }
 //7972
 
@@ -52,5 +52,31 @@ fn task2() {
     let mut count: i32 = 0;
     let mut pos: i32 = 50;
 
-    println!("Task2 Pw: {}", count);
+    for line in input.lines() {
+        let line = line.expect("Couldnt read line");
+        let rot_abs = line[1..].parse::<i32>().unwrap();
+
+        if line.starts_with('R') {
+            for _i in 0..rot_abs {
+                pos += 1;
+                if pos == 100 {
+                    pos = 0;
+                    count += 1;
+                }
+            }
+        } else if line.starts_with('L') {
+            for _i in 0..rot_abs {
+                pos -= 1;
+                if pos == -1 {
+                    pos = 99;
+                    count += 1;
+                }
+            }
+        }
+    }
+
+    println!("Task2 Pw: {}", count + task1());
 }
+
+//7850
+//7893
